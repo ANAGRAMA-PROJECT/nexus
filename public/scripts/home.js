8d84969f77a1b0f576d1943ca5e38c72a1a2b487
AlbertoMata <angelhz@comunidad.unam.mx>
2020-04-22 18:55:27 -0500

    Better formatting

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,27 +1,27 @@
-console.debug("Loading page... ");
+console.debug('Loading page... ');
 
 let pageState = {
-  sectionList: ["Inicio", "Noticias", "Proyectos", "Contacto", "Hackers"],
-  mainTitle: "Colectivo Anagrama",
-  defaultSection: "Inicio",
+	sectionList: ['Inicio', 'Noticias', 'Proyectos', 'Contacto', 'Hackers'],
+	mainTitle: 'Colectivo Anagrama',
+	defaultSection: 'Inicio'
 };
 
 getHomeContent = async () => {
-  let response = await fetch("templates/home-content.html");
-  let txt = await response.text();
+	let response = await fetch('templates/home-content.html');
+	let txt = await response.text();
 
-  let html = new DOMParser().parseFromString(txt, "text/html");
+	let html = new DOMParser().parseFromString(txt, 'text/html');
 
-  return html.querySelector("#main_content__home");
-}
-
-domContentLoadedHandler = async  (event) => {
-  let mainTitleElement = document.getElementsByClassName("main_title")[0];
-  let mainContentElement = document.getElementsByClassName("main_content")[0];
-
-  mainTitleElement.textContent = pageState.mainTitle;
-
-  mainContentElement.appendChild (await getHomeContent());
+	return html.querySelector('#main_content__home');
 };
 
-document.addEventListener("DOMContentLoaded", domContentLoadedHandler);
+domContentLoadedHandler = async (event) => {
+	let mainTitleElement = document.getElementsByClassName('main_title')[0];
+	let mainContentElement = document.getElementsByClassName('main_content')[0];
+
+	mainTitleElement.textContent = pageState.mainTitle;
+
+	mainContentElement.appendChild(await getHomeContent());
+};
+
+document.addEventListener('DOMContentLoaded', domContentLoadedHandler);
--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
-let express = require("express");
-let path = require("path");
+let express = require('express');
+let path = require('path');
 
 let app = express();
-let options = { index: "home.html" };
+let options = { index: 'home.html' };
 
-app.set("port", process.env.PORT || 5000);
+app.set('port', process.env.PORT || 5000);
 
-app.use('/', express.static(path.join(__dirname, "../public/"), options));
+app.use('/', express.static(path.join(__dirname, '../public/'), options));
 
-console.log (__dirname);
+console.log(__dirname);
 
-app.listen(app.get("port"), function () {
-  console.log("App listening at the port 5000!");
+app.listen(app.get('port'), function () {
+	console.log('App listening at the port 5000!');
 });

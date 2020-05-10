d89eba2fcd9267d7af0714f6b8a3bfd326ccef96
AlbertoMata <angelhz@comunidad.unam.mx>
2020-05-10 12:13:55 -0500

    Small fixes on readability

--- server/index.js
+++ server/index.js
@@ -1,14 +1,12 @@
-let express = require('express');
-let path = require('path');
+const express = require('express');
+const path = require('path');
 
-let app = express();
-let options = { index: 'main.html' };
+const app = express();
+const options = { index: 'main.html' };
 
 app.set('port', process.env.PORT || 5000);
 
 app.use('/', express.static(path.join(__dirname, '../public/'), options));
-
-console.log(__dirname);
 
 app.listen(app.get('port'), function () {
 	console.log('App listening at the port 5000!');

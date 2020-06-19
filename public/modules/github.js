070bc0f42913d6aef882328eba8cc7bb3d300921
AlbertoMata <angelhz@comunidad.unam.mx>
2020-06-18 20:23:28 -0500

    testing api callouts

--- public/modules/github.js
+++ public/modules/github.js
@@ -1,11 +1,11 @@
 async function getRepos(divResult) {
-	const url = 'http://api.github.com/search/repositories?q=stars:>100000';
-	const response = await fetch(url);
-	const result = await response.json();
-	result.items.forEach((i) => {
-		divResult.appendChild(document.createTextNode(i.full_name));
-		divResult.appendChild(document.createElement('br'));
-	});
+	// const url = 'http://api.github.com/search/repositories?q=stars:>100000';
+	// const response = await fetch(url);
+	// const result = await response.json();
+	// result.items.forEach((i) => {
+	// 	divResult.appendChild(document.createTextNode(i.full_name));
+	// 	divResult.appendChild(document.createElement('br'));
+	// });
 }
 
 export { getRepos };
